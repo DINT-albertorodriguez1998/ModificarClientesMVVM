217ee8e07247d01232e0f8a9ae5a03bbec860983
albertorodriguez1998 <alberto.rodriguezruiz@iesdoctorbalmis.com>
2020-01-17 10:16:37 +0100

    Agregar archivos de proyecto.

---
+++ ModificarClientesMVVM/MainWindowVM.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificarClientesMVVM
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        Tema_6Entities contexto;
+        public ObservableCollection<CLIENTE> listaClientes { get; set; }
+        public CLIENTE seleccionado { get; set; }
+
+
+        public MainWindowVM()
+        {
+            contexto = new Tema_6Entities();
+            contexto.CLIENTES.Load();
+
+            listaClientes = contexto.CLIENTES.Local;
+        }
+
+        public bool puedeEjecutar()
+        {
+            return seleccionado != null;
+        }
+
+        public void guardarCambios()
+        {
+            contexto.SaveChanges();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
